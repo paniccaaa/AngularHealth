f047ddc537e5e31a09b187b70d2c8f72b476f03b
paniccaaa <semaadamenko1@gmail.com>
2023-11-20 15:30:01 +0300

    appoint-t logic for upcoming/completed/canceled

--- src/app/components/appointment-today/appointment-today.component.ts
+++ src/app/components/appointment-today/appointment-today.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-today',
   templateUrl: './appointment-today.component.html',
-  styleUrls: ['./appointment-today.component.scss']
+  styleUrls: ['./appointment-today.component.scss'],
 })
 export class AppointmentTodayComponent {
-
+  @Input() eventTime: string = 'Upcoming';
+  //eventTime = this.eventSelected;
 }
