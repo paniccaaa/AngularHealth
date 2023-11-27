d8af9ca7066ff8b0e296e2ea315250ec8b0c8a82
paniccaaa <semaadamenko1@gmail.com>
2023-11-27 09:59:23 +0300

    complete template user-profile/dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -131,16 +131,18 @@ export class DashboardComponent {
   timings = new FormControl('');
 
   timingsList: string[] = [
+    '8:00 AM',
     '9:00 AM',
     '10:00 AM',
     '11:00 AM',
-    '12:00 PM',
+    '12:00 AM',
     '1:00 PM',
     '2:00 PM',
     '3:00 PM',
     '4:00 PM',
     '5:00 PM',
     '6:00 PM',
+    '7:00 PM',
   ];
 
   displayedColumns: string[] = [
--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.scss']
+  styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent {
-
+  selectedGender = '';
+  genders = ['Male', 'Female', 'Other'];
 }
