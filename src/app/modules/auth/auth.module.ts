3f75c8f64cdadea68fcd432540e3bba5b83ba291
paniccaaa <semaadamenko1@gmail.com>
2023-12-27 23:38:40 +0300

    delete extra imports

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,11 +1,10 @@
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { AuthComponent } from './pages/auth/auth.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthService } from './services/auth/auth.service';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material-module';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AuthComponent],
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,11 +1,10 @@
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { CategoriesComponent } from './components/categories/categories.component';
 import { CategoryService } from './services/category/category.service';
 import { CommonModule } from '@angular/common';
 import { DoctorCardComponent } from './components/doctor-card/doctor-card.component';
 import { DoctorDetailsComponent } from './pages/doctor-details/doctor-details.component';
 import { DoctorsService } from './services/doctors/doctors.service';
+import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './pages/home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { MaterialModule } from 'src/app/material-module';
